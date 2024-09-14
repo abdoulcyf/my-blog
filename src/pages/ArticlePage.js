c9280c9f29efc9275c5679883b11c1bb326c4392
ediallo <elhhabdouldiallo@gmail.com>
2024-09-14 13:23:32 +0100

    display article dynamically

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,8 +1,19 @@
 import { useParams } from "react-router-dom";
 
+import articles from "./article-content";
+
 const ArticlePage = () => {
   const { articleId } = useParams();
-  return <h1>This is the Article Page with id: {articleId}</h1>;
+
+  const article = articles.find((article) => article.name === articleId);
+  return (
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph) => (
+        <p>{paragraph}</p>
+      ))}
+    </>
+  );
 };
 
 export default ArticlePage;
