398236403d650ed6be751e50ef4c3e1f1ed640d9
ediallo <elhhabdouldiallo@gmail.com>
2024-09-14 13:58:11 +0100

    adds home page content

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,40 @@
 const HomePage = () => {
-  return <h1>This is the Home Page!</h1>;
+  return (
+    <>
+      <h1>Welcome to EDA's Development Hub</h1>
+      <h2>Crafting Digital Experiences from Front to Back</h2>
+      <p>
+        I am a full stack developer dedicated to building seamless,
+        high-performance web applications that blend innovative design with
+        robust functionality. With expertise in both front-end and back-end
+        technologies, I bring your digital ideas to life with precision and
+        creativity.
+      </p>
+
+      <h2>Services Offered</h2>
+      <ul>
+        <li>
+          <b>Front-End Development:</b> Creating intuitive, responsive
+          interfaces using modern technologies like HTML5, CSS3, and JavaScript
+          frameworks (React, Angular, or Vue.js).
+        </li>
+        <li>
+          <b>Back-End Development:</b> Developing secure and scalable back-end
+          systems with Node.js, Express, and various server-side tools.
+        </li>
+        <li>
+          <b>Full Stack Integration:</b> DProviding comprehensive solutions to
+          ensure smooth interaction between front-end and back-end components.
+        </li>
+      </ul>
+      <h3>Let's Build Together</h3>
+      <p>
+        Ready to enhance your digital presence? Get in touch for a consultation,
+        and let's turn your vision into reality.
+      </p>
+      <h3>Contact me</h3>
+    </>
+  );
 };
 
 export default HomePage;
