b4b0bba4ee4afd78f67a73d788c7e53ded447e7f
ediallo <elhhabdouldiallo@gmail.com>
2024-09-14 14:25:05 +0100

    gets rid of double import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavBar from "./NavBar";
 import HomePage from "./pages/HomePage";
