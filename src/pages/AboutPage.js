fa9d04b7abfdb822b4d853e695f2bb8adaf8a813
ediallo <elhhabdouldiallo@gmail.com>
2024-09-14 13:57:20 +0100

    adds about page content

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,5 +1,54 @@
 const AboutPage = () => {
-  return <h1>This is the About Page!</h1>;
+  return (
+    <>
+      <h1>About Me</h1>
+      <p>
+        I'm Elhadj Abdoul Diallo, a passionate full stack developer with a knack
+        for crafting dynamic, user-centric web applications. With a background
+        in both front-end and back-end development, I specialize in creating
+        digital solutions that are not only visually appealing but also highly
+        functional and scalable.
+      </p>
+      <h2>My Journey</h2>
+      <p>
+        I started my journey in web development [X years] ago, driven by a
+        fascination with how the web can connect people and ideas. Over the
+        years, I've honed my skills in various programming languages,
+        frameworks, and tools, enabling me to tackle projects of varying
+        complexity with confidence.
+      </p>
+      <h2>Skills & Expertise</h2>
+
+      <ul>
+        <li>
+          <b>Front-End Technologies:</b> HTML, CSS, JavaScript, React, Angular,
+          Vue.js.
+        </li>
+        <li>
+          <b>Back-End Technologies:</b> Node.js, Express, MongoDB, SQL, RESTful
+          APIs
+        </li>
+        <li>
+          <b>Other Skills</b> Responsive Design, Version Control (Git), Testing
+          & Debugging
+        </li>
+      </ul>
+
+      <h2>My Philosophy</h2>
+      <p>
+        I believe in creating digital experiences that are not just functional
+        but also enjoyable for users. This means focusing on performance,
+        usability, and aesthetics to deliver products that truly make an impact.
+      </p>
+
+      <h2>Beyond Code</h2>
+      <p>
+        When I'm not coding, I enjoy [briefly mention a hobby or interest, e.g.,
+        "exploring the outdoors," "learning about new tech trends," "playing the
+        guitar"] which helps me bring a fresh perspective to my work.
+      </p>
+    </>
+  );
 };
 
 export default AboutPage;
