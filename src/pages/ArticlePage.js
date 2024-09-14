b3c6eb55ea8f2740e66eaafa3d2efe5d21c4d277
ediallo <elhhabdouldiallo@gmail.com>
2024-09-14 13:05:18 +0100

    removes console.log

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -2,7 +2,6 @@ import { useParams } from "react-router-dom";
 
 const ArticlePage = () => {
   const { articleId } = useParams();
-  console.log(articleId);
   return <h1>This is the Article Page with id: {articleId}</h1>;
 };
 
