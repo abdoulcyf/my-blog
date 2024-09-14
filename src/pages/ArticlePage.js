209242c43fd42c09e9f3771af2e6e9e86c00bb7a
ediallo <elhhabdouldiallo@gmail.com>
2024-09-15 00:44:25 +0100

    load data from db using axios and use CommentList component in article

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,22 @@
 import { useParams } from "react-router-dom";
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
+import CommentList from "../components/commentList";
 
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
   const { articleId } = useParams();
+
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      const response = await axios.get(`/api/articles/${articleId}`);
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    };
+    loadArticleInfo();
+  }, [articleId]);
 
   const article = articles.find((article) => article.name === articleId);
 
@@ -18,14 +30,11 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvotes</p>
       {article.content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
-
-      {/* <h1>{otherArticle.title}</h1>
-      {otherArticle.content.map((paragraph, index) => (
-        <p key={index}>{paragraph}</p>
-      ))} */}
+      <CommentList comments={articleInfo.comments} />
     </>
   );
 };
