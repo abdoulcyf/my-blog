9e0a60c207583a56a5dec8854efcb4b53bc2def0
ediallo <elhhabdouldiallo@gmail.com>
2024-09-16 23:06:21 +0100

    installs firebase package

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYlYubTG4zfTBPbkEao_vo5tG8FsG2y24",
+  authDomain: "my-blog-website-f4052.firebaseapp.com",
+  projectId: "my-blog-website-f4052",
+  storageBucket: "my-blog-website-f4052.appspot.com",
+  messagingSenderId: "597337653176",
+  appId: "1:597337653176:web:32664279246a5dfffe194d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
