50e1e6d4f17f9139123d6d36db700b6cba4c936a
ediallo <elhhabdouldiallo@gmail.com>
2024-09-16 23:28:21 +0100

    call useUser hook in article page

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -5,10 +5,13 @@ import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 import CommentList from "../components/commentList";
 import AddCommentForm from "../components/AddComment-From";
+import useUser from "../hooks/useUser";
 
 const ArticlePage = () => {
   const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
   const { articleId } = useParams();
+
+  const { user, isLoading } = useUser();
 
   useEffect(() => {
     const loadArticleInfo = async () => {
@@ -39,16 +42,24 @@ const ArticlePage = () => {
     <>
       <h1>{article.title}</h1>
       <div className="upvote-section">
-        <button onClick={addUpvote}>Upvote</button>
+        {user ? (
+          <button onClick={addUpvote}>Upvote</button>
+        ) : (
+          <button>Log in to upvote</button>
+        )}
         <p>This article has {articleInfo.upvotes} upvotes</p>
       </div>
       {article.content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
-      <AddCommentForm
-        articleName={articleId}
-        onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
-      />
+      {user ? (
+        <AddCommentForm
+          articleName={articleId}
+          onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
+        />
+      ) : (
+        <button>Login to add a comment</button>
+      )}
       <CommentList comments={articleInfo.comments || []} />
     </>
   );
