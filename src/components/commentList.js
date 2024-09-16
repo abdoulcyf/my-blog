271072a02dd5b2bf5cbf96f2e5c5aed1423676d0
ediallo <elhhabdouldiallo@gmail.com>
2024-09-16 23:07:48 +0100

    handles cases when postedBy and text in comments array are empty

--- src/components/commentList.js
+++ src/components/commentList.js
@@ -2,12 +2,14 @@ const CommentList = ({ comments }) => {
   return (
     <>
       <h3>Comments:</h3>
-      {comments.map((comment, index) => (
-        <div className="comment" key={index}>
-          <h4>{comment.postedBy}</h4>
-          <p>{comment.text}</p>
-        </div>
-      ))}
+      {comments.map((comment, index) =>
+        comment.postedBy === "" || comment.text === "" ? null : (
+          <div className="comment" key={index}>
+            <h4>{comment.postedBy}</h4>
+            <p>{comment.text}</p>
+          </div>
+        )
+      )}
     </>
   );
 };
